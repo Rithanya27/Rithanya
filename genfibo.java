25aef4aadc73cd127e8e062ceb8b37f212f6ffa0
Rithanya27 <rithanya27@gmail.com>
2016-07-03 13:04:07 +0530

    Update genfibo.java

--- genfibo.java
+++ genfibo.java
@@ -3,24 +3,30 @@ package fibo;
 
 import java.util.Scanner;
 
-public class Fibo {
-
-    public static void main(String args[])  
-{    
- int n1=0,n2=1,n3,i,count;
- System.out.println("enter the no");
+public class gen {
+     public static void main(String[] args) {
+               
+                int limit;
+                 System.out.println("enter the no");
         Scanner ob=new Scanner(System.in);
-        count=ob.nextInt();
- System.out.print(n1+" "+n2);    
-    
- for(i=2;i<count;++i)    
- {    
-  n3=n1+n2;    
-  System.out.print(" "+n3);    
-  n1=n2;    
-  n2=n3;    
- }    
-    }
-    
+        limit=ob.nextInt();
+               
+                long[] series = new long[limit];
+               
+                series[0] = 0;
+                series[1] = 1;
+               
+            
+                for(int i=2; i < limit; i++){
+                        series[i] = series[i-1] + series[i-2];
+                }
+               
+                
+               
+                System.out.println("Fibonacci Series upto " + limit);
+                for(int i=0; i< limit; i++){
+                        System.out.print(series[i] + " ");
+                }
+        }
     
 }
